038e87502650fc02232bc2a6513b5f8d74daab5d
dausi15 <dausi15@student.sdu.dk>
2017-03-31 14:45:53 +0200

    Empty Core modul tilføjet m. dependencies

---
+++ LabyrinthShooter/Core/src/main/java/gruppe5/core/main/Game.java
@@ -0,0 +1,54 @@
+package gruppe5.core.main;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+//import dk.sdu.mmmi.cbse.common.data.Entity;
+//import dk.sdu.mmmi.cbse.common.data.GameData;
+//import dk.sdu.mmmi.cbse.common.data.World;
+//import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+//import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+//import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+//import dk.sdu.mmmi.cbse.core.managers.GameInputProcessor;
+import org.openide.util.Lookup;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
+
+public class Game implements ApplicationListener {
+
+    private static OrthographicCamera cam;
+//    private final GameData gameData = new GameData();
+//    private World world = new World();
+
+    @Override
+    public void create() {
+    }
+
+    @Override
+    public void render() {
+    }
+
+    private void update() {
+    }
+
+    private void draw() {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
