caf0c3ff1c30888c82cd57061f51a017f30dea2d
Marc Kristensen <marcndkk@gmail.com>
2017-04-04 14:01:53 +0200

    Fixed CommonWeapon

---
+++ LabyrinthShooter/CommonWeapon/src/main/java/gruppe5/common/weapon/Weapon.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gruppe5.common.weapon;
+
+import gruppe5.common.data.Entity;
+
+/**
+ *
+ * @author marcn
+ */
+public class Weapon extends Entity{
+    private Entity owner;
+    public Entity getOwner() {
+        return owner;
+    }
+    
+    public void setOwner(Entity entity) {
+        this.owner = entity;
+    }
+}
