da9705082f6a3d05f4b907a6e09d5e15b77ef2c6
Marc Kristensen <marcndkk@gmail.com>
2017-04-05 11:26:27 +0200

    BulletCreator created

---
+++ LabyrinthShooter/Bullet/src/main/java/gruppe5/bullet/BulletCreator.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gruppe5.bullet;
+
+import gruppe5.common.bullet.BulletSPI;
+import gruppe5.common.data.Entity;
+import gruppe5.common.data.GameData;
+import org.openide.util.lookup.ServiceProvider;
+
+
+@ServiceProvider(service = BulletSPI.class)
+/**
+ *
+ * @author marcn
+ */
+public class BulletCreator implements BulletSPI{
+
+    @Override
+    public Entity createBullet(GameData gameData, Entity creator) {
+        Entity bullet = new Entity();
+        bullet.setAcceleration(1000);
+        bullet.setCollidable(true);
+        bullet.setDamage(1);
+        bullet.setDeacceleration(0);
+        bullet.setDx(creator.getDx());
+        bullet.setDy(creator.getDy());
+        bullet.setExpiration(3);
+        bullet.setLife(1);
+        bullet.setMaxSpeed(300);
+        bullet.setPosition(creator.getX(), creator.getY());
+        bullet.setRadians(creator.getRadians());
+        bullet.setRadius(8);
+        bullet.setRotationSpeed(0);
+        
+        return bullet;
+    }
+    
+}
