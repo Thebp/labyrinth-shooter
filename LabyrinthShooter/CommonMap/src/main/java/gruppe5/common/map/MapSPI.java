f5c89d3fd5e1cd30e8a6cc84f8ddaff463a76f5e
Nick Kjeldsen <nkjel14@student.sdu.dk>
2017-04-18 11:29:20 +0200

    IGameInitService added, MapSPI changed

--- LabyrinthShooter/CommonMap/src/main/java/gruppe5/common/map/MapSPI.java
+++ LabyrinthShooter/CommonMap/src/main/java/gruppe5/common/map/MapSPI.java
@@ -17,4 +17,14 @@ public interface MapSPI {
      * @return A list of all MapNodes in the game.
      */
     public List<MapNode> getMap();
+    /**
+     * Returns all center nodes, i.e. nodes that do not touch walls
+     * @return 
+     */
+    public List<MapNode> getCenterMapNodes();
+    /**
+     * Finds and reserves a MapNode such that this MapNode won't be returned again by this function
+     * @return 
+     */
+    public MapNode getRandomSpawnNode();
 }
