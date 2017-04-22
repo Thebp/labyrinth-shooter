04153671350e1497ea0591883872bd3bf51937d9
Marc Kristensen <marcndkk@gmail.com>
2017-04-22 18:37:33 +0200

    Gave Enemy class a variable Entity target

--- LabyrinthShooter/Enemy/src/main/java/gruppe5/enemy/Enemy.java
+++ LabyrinthShooter/Enemy/src/main/java/gruppe5/enemy/Enemy.java
@@ -3,5 +3,13 @@ package gruppe5.enemy;
 import gruppe5.common.data.Entity;
 
 public class Enemy extends Entity{
+    private Entity target;
     
+    public Entity getTarget() {
+        return target;
+    }
+    
+    public void setTarget(Entity entity) {
+        this.target = entity;
+    }
 }
