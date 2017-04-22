5b1b3ac4403970b0ae84a0f33138c311da1b8267
Marc Kristensen <marcndkk@gmail.com>
2017-04-22 19:43:13 +0200

    TargetNode added to Enemy

--- LabyrinthShooter/Enemy/src/main/java/gruppe5/enemy/Enemy.java
+++ LabyrinthShooter/Enemy/src/main/java/gruppe5/enemy/Enemy.java
@@ -1,9 +1,11 @@
 package gruppe5.enemy;
 
 import gruppe5.common.data.Entity;
+import gruppe5.common.map.MapNode;
 
 public class Enemy extends Entity{
     private Entity target;
+    private MapNode targetNode;
     
     public Entity getTarget() {
         return target;
@@ -11,5 +13,13 @@ public class Enemy extends Entity{
     
     public void setTarget(Entity entity) {
         this.target = entity;
+    }
+    
+    public MapNode getTargetNode() {
+        return targetNode;
+    }
+    
+    public void setTargetNode(MapNode targetNode) {
+        this.targetNode = targetNode;
     }
 }
