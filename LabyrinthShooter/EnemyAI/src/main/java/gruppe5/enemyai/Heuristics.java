a5352ba288a74aa20826f4f6ac9379e1c308b1aa
Gyhuji93 <gyhuji.93@gmail.com>
2017-05-15 12:05:11 +0200

    Added parent

--- LabyrinthShooter/EnemyAI/src/main/java/gruppe5/enemyai/Heuristics.java
+++ LabyrinthShooter/EnemyAI/src/main/java/gruppe5/enemyai/Heuristics.java
@@ -5,10 +5,44 @@
  */
 package gruppe5.enemyai;
 
+import gruppe5.common.node.MapNode;
+
+
+
 /**
  *
  * @author Gyhuji
  */
-public class Heuristics {
+public class Heuristics{
+    int gCost;
+    int hCost;
+    MapNode parent;
+
+    public MapNode getParent() {
+        return parent;
+    }
+
+    public void setParent(MapNode newparent) {
+        this.parent = newparent;
+    }
+
+    public int getgCost() {
+        return gCost;
+    }
+
+    public void setgCost(int newgCost) {
+        this.gCost = newgCost;
+    }
+
+    public int gethCost() {
+        return hCost;
+    }
+
+    public void sethCost(int newhCost) {
+        this.hCost = newhCost;
+    }
     
+    public float fCost(){
+        return gCost + hCost;
+    }
 }
