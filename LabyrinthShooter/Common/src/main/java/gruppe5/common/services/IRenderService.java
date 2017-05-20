afc5aebb64b187cdf359511cc26dd68ce624ff12
dausi15 <dausi15@student.sdu.dk>
2017-05-20 12:53:01 +0200

    Deleted interface not used

--- LabyrinthShooter/Common/src/main/java/gruppe5/common/services/IRenderService.java
+++
@@ -1,9 +0,0 @@
-package gruppe5.common.services;
-
-import gruppe5.common.data.GameData;
-import gruppe5.common.data.World;
-
-public interface IRenderService  {
-        void create(GameData gameData, World world);
-        void render(GameData gameData, World world);
-}
